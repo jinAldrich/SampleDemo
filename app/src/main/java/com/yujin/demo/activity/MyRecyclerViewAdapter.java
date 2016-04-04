09993077d978d0a137ed20ddd66ec1cde8c86d95
jin.yu <amtforg@sina.com>
2016-04-04 14:30:25 +0800

    2016-4-4 add attribute "snap" for CollapsingToolbarLayout

--- app/src/main/java/com/yujin/demo/activity/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/yujin/demo/activity/MyRecyclerViewAdapter.java
@@ -1,10 +1,12 @@
 package com.yujin.demo.activity;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.yujin.demo.R;
@@ -12,7 +14,12 @@ import com.yujin.demo.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
+    public static final String TAG = MyRecyclerViewAdapter.class.getSimpleName();
+
     private List<Integer> datas;
     private Context context;
     private List<Integer> lists;
@@ -33,6 +40,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.layout_item, parent, false);
+        ButterKnife.bind(this, view);
         MyViewHolder viewHolder = new MyViewHolder(view);
         return viewHolder;
     }
@@ -42,7 +50,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
         ViewGroup.LayoutParams params = holder.itemView.getLayoutParams();
         params.height = lists.get(position);//把随机的高度赋予item布局
         holder.itemView.setLayoutParams(params);
-        holder.mTextView.setText(position+"");
+        holder.itemTvTitle.setText(position + "");
     }
 
     @Override
@@ -52,9 +60,16 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyViewHolder> {
 }
 
 class MyViewHolder extends RecyclerView.ViewHolder {
-    TextView mTextView;
+    @Bind(R.id.item_iv)
+    ImageView itemIv;
+    @Bind(R.id.item_tv_title)
+    TextView itemTvTitle;
+    @Bind(R.id.item_tv_content)
+    TextView itemTvContent;
+    @Bind(R.id.cardview)
+    CardView cardview;
     public MyViewHolder(View itemView) {
         super(itemView);
-        mTextView = (TextView) itemView.findViewById(R.id.item_tv);
+        ButterKnife.bind(this, itemView);
     }
 }
